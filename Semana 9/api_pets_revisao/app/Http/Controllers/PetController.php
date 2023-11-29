0bbee22cad417205983a570d36c06e26de3f0fe4
Caroline Sampaio <caroline_08022@hotmail.com>
2023-11-28 22:24:43 -0300

    feat: adiciona exemplos aula 2 - envio email

--- Semana 9/api_pets_revisao/app/Http/Controllers/PetController.php
+++ Semana 9/api_pets_revisao/app/Http/Controllers/PetController.php
@@ -2,20 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\SendWelcomePet;
 use App\Models\Pet;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Symfony\Component\HttpFoundation\Response;
 
 class PetController extends Controller {
     use HttpResponses;
 
-    public function index() {
+    public function index(Request $request) {
         try {
-            $pets = Pet::all();
-            return $pets;
+
+            // pegar os dados que foram enviados via query params
+            $filters = $request->query();
+
+            // inicializa uma query
+            $pets = Pet::query();
+
+            // verifica se filtro
+            if ($request->has('name') && !empty($filters['name'])) {
+                $pets->where('name', 'ilike', '%' . $filters['name'] . '%');
+            }
+
+            if ($request->has('age') && !empty($filters['age'])) {
+                $pets->where('age', $filters['age']);
+            }
+
+            if ($request->has('size') && !empty($filters['size'])) {
+                $pets->where('size', $filters['size']);
+            }
+
+            if ($request->has('weight') && !empty($filters['weight'])) {
+                $pets->where('weight', $filters['weight']);
+            }
+
+            // retorna o resultado
+            $columnOrder = $request->has('order') && !empty($filters['order']) ?  $filters['order'] : 'name';
+
+            return $pets->orderBy($columnOrder)->get();
         } catch (\Exception $exception) {
-            return $this->error($exception->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function store(Request $request) {
+        try {
+            // rebecer os dados via body
+            $data = $request->all();
+
+            $request->validate([
+                'name' => 'required|string|max:150',
+                'age' => 'int',
+                'weight' => 'numeric',
+                'size' => 'required|string|in:SMALL,MEDIUM,LARGE,EXTRA_LARGE', // melhorar validacao para enum
+                'breed_id' => 'required|int',
+                'specie_id' => 'required|int'
+            ]);
+
+            $pet = Pet::create($data);
+
+            Mail::to('caroline_sampaio@estudante.sesisenai.org.br', 'Caroline Sampaio')
+                ->send(new SendWelcomePet($pet->name, 'Caroline Sampaio'));
+
+            return $pet;
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
 }
--- Semana 9/api_pets_revisao/routes/api.php
+++ Semana 9/api_pets_revisao/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BreedController;
+use App\Http\Controllers\PetController;
 use App\Http\Controllers\SpecieController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,7 @@ Route::get('breeds', [BreedController::class, 'index']);
 
 Route::post('species', [SpecieController::class, 'store']);
 Route::get('species', [SpecieController::class, 'index']);
+
+
+Route::post('pets', [PetController::class, 'store']);
+Route::get('pets', [PetController::class, 'index']);
