c0a6727569d33b75d502d877dc9d6e758fcd8f55
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-03 21:31:35 -0300

    feat: adiciona ex 3 - markers

--- Semana 7/exercicios_semana_7/routes/api.php
+++ Semana 7/exercicios_semana_7/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AvaliationController;
+use App\Http\Controllers\MarkerController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductMarkerController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/products', [ProductController::class, 'store']);
@@ -8,3 +11,11 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+Route::post('/markers', [MarkerController::class, 'store']);
+Route::get('/markers', [MarkerController::class, 'index']);
+Route::delete('/markers/{id}', [MarkerController::class, 'destroy']);
+
+Route::post('/product_markers', [ProductMarkerController::class, 'store']);
+Route::get('/product_markers', [ProductMarkerController::class, 'index']);
+Route::delete('/product_markers/{id}', [ProductMarkerController::class, 'destroy']);
