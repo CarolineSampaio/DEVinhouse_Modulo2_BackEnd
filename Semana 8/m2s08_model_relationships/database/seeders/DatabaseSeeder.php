b4d0322b6ceb665b09c0e643dcf6a68dc40419c3
Caroline Sampaio <caroline_08022@hotmail.com>
2023-12-03 23:13:32 -0300

    feat: adiciona ex crud e seeder generos

--- Semana 8/m2s08_model_relationships/database/seeders/DatabaseSeeder.php
+++ Semana 8/m2s08_model_relationships/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Gender as GenderModel;
+use App\Models\User as UserModel;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        if(!UserModel::count())
+        {
+            UserModel::factory(10)->create();
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        if(!GenderModel::count())
+        {
+            $this->call([GenderSeeder::class]);
+        }
     }
 }
